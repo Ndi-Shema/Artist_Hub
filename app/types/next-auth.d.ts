a53e4e2ef7a2b3794138e9fbee8d7a9eb8c69ffc
Ndi-Shema <f.shema@alustudent.com>
2025-03-26 01:08:50 +0200

    profile fix and type error fixes

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -1,4 +1,6 @@
-import NextAuth from "next-auth";
+// app/types/next-auth.d.ts
+
+import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
@@ -6,10 +8,10 @@ declare module "next-auth" {
       id: string;
       email: string;
       name: string;
-    };
+    } & DefaultSession["user"];
   }
 
-  interface User {
+  interface User extends DefaultUser {
     id: string;
     email: string;
     name: string;
